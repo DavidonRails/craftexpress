c9f2448849194f40eb10f37acea7d5a0c38fe7a9
davidonrails <david.ruby.rails@gmail.com>
2016-01-04 10:01:26 +0100

    front end html parse

--- routes/gethook.js
+++ routes/gethook.js
@@ -1,29 +1,85 @@
 var express = require('express');
+
+var http = require("http");
+var https = require("https");
+var htmlparser = require("htmlparser2");
+
 var router = express.Router();
 
-var quotes = [
-  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
-];
 /*
 var mongoose = require( 'mongoose' );
 var ImageCollection     = mongoose.model( 'Images' );
 */
 /* GET home page. */
 router.post('/', function(req, res, next) {
-
 	var front_url = req.param('url');
-  	// res.json(quotes);
+	
+	front_url = "http://http://whitehouse.prod51.fr/wp/";
 
     res.setHeader('Access-Control-Allow-Origin', '*');
-
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
 
-  	res.send("Frontend:" + front_url);
+	if(front_url === undefined) {
+	    res.send({message: "Frontend URL cannot be undefined"});
+	}
+
+	var urlPrefix = front_url.match(/.*?:\/\//g);
+	front_url = front_url.replace(/.*?:\/\//g, "");
+
+	var options = {
+	    hostname: front_url
+	};
+
+	if(urlPrefix !== undefined && urlPrefix !== null && urlPrefix[0] === "https://") {
+	    options.port = 443;
+	    https.get(options, function(result) {
+	        processResponse(result);
+	    }).on('error', function(e) {
+	        res.send({message: e.message});
+	    });
+	} else {
+	    options.port = 80;
+	    http.get(options, function(result) {
+	        processResponse(result);
+	    }).on('error', function(e) {
+	        res.send({message: e.message});
+	    });
+	}
+
+	var processResponse = function(result) {
+	    var data = "";
+	    result.on("data", function(chunk) {
+	        data += chunk;
+	    });
+	    var tags = [];
+	    var tagsCount = {};
+	    var tagsWithCount = [];
+	    result.on("end", function(chunk) {
+	        var parser = new htmlparser.Parser({
+	            onopentag: function(name, attribs) {
+	                if(tags.indexOf(name) === -1) {
+	                    tags.push(name);
+	                    tagsCount[name] = 1;
+	                } else {
+	                    tagsCount[name]++;
+	                }
+	            },
+	            onend: function() {
+	                for(var i = 0; i < tags.length; i++) {
+	                    tagsWithCount.push({name: tags[i], count: tagsCount[tags[i]]});
+	                }
+	            }
+	        }, {decodeEntities: true});
+	        parser.write(data);
+	        parser.end();
+	        res.send({website: req.query.url, port: options.port, data: data, tags: tagsWithCount});
+	    });
+	}
+
+  	// res.send("Frontend:" + front_url);
 });
+
+
 
 module.exports = router;
