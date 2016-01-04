379c82a3c6a103f2085102027bb2f88f9898acd5
davidonrails <david.ruby.rails@gmail.com>
2016-01-04 10:40:10 +0100

    HTML Parse using Cheerio, Request, JSDom, Http, https, Htmlparse2

--- routes/gethook.js
+++ routes/gethook.js
@@ -1,8 +1,7 @@
 var express = require('express');
 
-var http = require("http");
-var https = require("https");
-var htmlparser = require("htmlparser2");
+var request = require("request");
+var cheerio = require("cheerio");
 
 var router = express.Router();
 
@@ -23,59 +22,20 @@ router.post('/', function(req, res, next) {
 	if(front_url === undefined) {
 	    res.send({message: "Frontend URL cannot be undefined"});
 	}
-/*
-	var urlPrefix = front_url.match(/.*?:\/\//g);
-	front_url = front_url.replace(/.*?:\/\//g, "");
-*/
-	var options = {
-	    hostname: front_url
-	};
-/*
-	if(urlPrefix !== undefined && urlPrefix !== null && urlPrefix[0] === "https://") {
-	    options.port = 443;
-	    https.get(options, function(result) {
-	        processResponse(result);
-	    }).on('error', function(e) {
-	        res.send({message: e.message});
-	    });
-	} else {*/
-	    //options.port = 80;
-	    http.get(options, function(result) {
-	        processResponse(result);
-	    }).on('error', function(e) {
-	        res.send({message: e.message});
-	    });
-	// }
 
-	var processResponse = function(result) {
-	    var data = "";
-	    result.on("data", function(chunk) {
-	        data += chunk;
-	    });
-	    var tags = [];
-	    var tagsCount = {};
-	    var tagsWithCount = [];
-	    result.on("end", function(chunk) {
-	        var parser = new htmlparser.Parser({
-	            onopentag: function(name, attribs) {
-	                if(tags.indexOf(name) === -1) {
-	                    tags.push(name);
-	                    tagsCount[name] = 1;
-	                } else {
-	                    tagsCount[name]++;
-	                }
-	            },
-	            onend: function() {
-	                for(var i = 0; i < tags.length; i++) {
-	                    tagsWithCount.push({name: tags[i], count: tagsCount[tags[i]]});
-	                }
-	            }
-	        }, {decodeEntities: true});
-	        parser.write(data);
-	        parser.end();
-	        res.send({website: req.query.url, port: options.port, data: data, tags: tagsWithCount});
-	    });
-	}
+	request({
+	  uri: "http://www.sitepoint.com",
+	}, function(error, response, body) {
+	  var $ = cheerio.load(body);
+
+	  $(".Footer_copyright").each(function() {
+	    var link = $(this);
+	    var text = link.html();
+
+	    console.log(text);
+	  });
+	  res.send(text);
+	});
 
   	// res.send("Frontend:" + front_url);
 });
